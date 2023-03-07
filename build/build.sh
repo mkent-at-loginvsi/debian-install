29d357d1564e643c0feb92da144353f006f3e148
Michael Kent <m.kent@loginvsi.com>
2023-03-07 12:55:26 -0800

    Initial Port
    Port over all files

---
+++ build/build.sh
@@ -0,0 +1,166 @@
+#!/bin/bash
+echo "----------------------------------------------------------------"
+echo "The script you are running has basename $( basename -- "$0"; ), dirname $( dirname -- "$0"; )";
+echo "The present working directory is $( pwd; )";
+echo "----------------------------------------------------------------"
+export WORK_DIR="$PWD"
+export OUTPUT_DIR=$WORK_DIR/..
+
+echo "----------------------------------------------------------------"
+echo "Install Archive will be written to $OUTPUT_DIR"
+echo "----------------------------------------------------------------"
+
+# Disk space check
+FREE=`df -k / --output=avail "$PWD" | tail -n1`   # df -k not df -h
+if [[ $FREE -lt 38990768 ]]; then               # 40G = 26*1024*1024k (Kibibyte)
+     # less than 26GBs free!
+     echo "----------------------------------------------------------------"
+     echo "The installation requires 40GB Free on the root partition (/)"
+     echo "----------------------------------------------------------------"
+     #exit
+fi
+
+# Create Build Directory
+echo "----------------------------------------------------------------"
+echo "Creating Build Directory"
+echo "----------------------------------------------------------------"
+dir="build-$(date +%Y_%m_%d_%H_%M_%S)"
+export BUILD_DIR="$PWD/$dir"
+out_dir="appliance"
+mkdir $dir
+echo "----------------------------------------------------------------"
+echo "Relative Build Direcory: $dir"
+echo "Full Path Build Directory: $BUILD_DIR"
+echo "----------------------------------------------------------------"
+
+# Download ISO
+isoFile="LoginEnterprise-4.8.10.iso"
+echo "----------------------------------------------------------------"
+echo "Downloading Update ISO to $BUILD_DIR/$isoFile"
+echo "----------------------------------------------------------------"
+
+if ! [ -f $BUILD_DIR/$isoFile ]; then
+  curl -o $BUILD_DIR/$isoFile https://loginvsidata.s3.eu-west-1.amazonaws.com/LoginPI3_ISO/public/$isoFile
+fi
+
+# Mount ISO
+echo "----------------------------------------------------------------"
+echo "Check if ISO is mounted"
+echo "----------------------------------------------------------------"
+if ! [ -d /media/iso ]; then
+  sudo mkdir /media/iso
+fi
+
+if ! [ -d /media/iso/update ]; then
+  sudo mount $BUILD_DIR/$isoFile /media/iso -o loop
+fi
+
+# Fail if ISO doesn't exist
+echo "----------------------------------------------------------------"
+echo "Checking if ISO Mounted"
+echo "----------------------------------------------------------------"
+if ! [ -d /media/iso/update ]; then
+  echo "Mount failed"
+  exit 1
+fi
+
+# Download Appliance VHD zip
+applianceFile="AZ-VA-LoginEnterprise-4.8.10.zip"
+echo "----------------------------------------------------------------"
+echo "Downloading Virtual Appliance to $BUILD_DIR/$applianceFile"
+echo "----------------------------------------------------------------"
+
+if ! [ -f $BUILD_DIR/$applianceFile ]; then
+  curl -o $BUILD_DIR/$applianceFile https://loginvsidata.s3.eu-west-1.amazonaws.com/LoginEnterprise/VirtualAppliance/$applianceFile
+fi
+
+# Unzip VHD
+echo "----------------------------------------------------------------"
+echo "Unzipping Virtual Appliance VHD $BUILD_DIR/$applianceFile"
+echo "----------------------------------------------------------------"
+sudo apt install -y unzip
+if ! [ -f $BUILD_DIR/"${applianceFile/zip/vhd}" ]; then
+  unzip -d $BUILD_DIR $BUILD_DIR/$applianceFile
+fi
+
+# Mount VHD
+echo "----------------------------------------------------------------"
+echo "Mounting Virtual Hard Drive"
+echo "----------------------------------------------------------------"
+sudo apt install -y libguestfs-tools
+sudo mkdir /mnt/vhd
+sudo chmod 777 /mnt/vhd
+
+mountpath="$BUILD_DIR"
+LIBGUESTFS_BACKEND=direct
+export LIBGUESTFS_BACKEND
+
+if ! [ -d /mnt/vhd/loginvsi ]; then
+  guestmount --add $mountpath/AZ-VA-LoginEnterprise-4.8.10.vhd --ro /mnt/vhd/ -m /dev/sda1
+fi
+
+# Fail if VHD doesn't exist
+echo "----------------------------------------------------------------"
+echo "Checking if VHD Mounted"
+echo "----------------------------------------------------------------"
+if ! [ -d /mnt/vhd/loginvsi ]; then
+  echo "Mount failed"
+  exit 1
+fi
+
+# Copy Files and Directories to output dir
+echo "----------------------------------------------------------------"
+echo "Copying Files to archive"
+echo "----------------------------------------------------------------"
+build_out=$BUILD_DIR/$out_dir
+mkdir $build_out
+
+# Copy Login Enterprise Installation
+cp -r /mnt/vhd/loginvsi $build_out/
+
+# Copy Login Enterprise Service
+mkdir -p $build_out/etc/systemd/system/
+cp -f /mnt/vhd/etc/systemd/system/loginvsid.service $build_out/etc/systemd/system/loginvsid.service
+
+# Copy rpms
+mkdir -p $build_out/rpms
+cp -r $BUILD_DIR/*.rpm $build_out/rpms/
+
+# Copy Docker Images
+imageFile="images.tar.gz"
+mkdir -p $build_out/images
+cp -r /media/iso/$imageFile $build_out/images/
+
+#Copy Login Enterprise Service Watcher
+cp -f /mnt/vhd/etc/systemd/system/pi_guard.service $build_out/etc/systemd/system/pi_guard.service
+
+#Copy firstrun, daemon and Menuing
+mkdir -p $build_out/usr/bin
+cp -f /mnt/vhd/usr/bin/loginvsid $build_out/usr/bin/loginvsid
+curl -o $build_out/usr/bin/pdmenu https://github.com/mkent-at-loginvsi/rhel-install/raw/main/pdmenu/pdmenu.rhel
+
+# Fix firstrun
+
+#zip up appliance build
+echo "----------------------------------------------------------------"
+echo "Packaging Archive"
+echo "----------------------------------------------------------------"
+cd $BUILD_DIR
+tar -czvf $out_dir.tar.gz $out_dir
+mv -v $out_dir.tar.gz $OUTPUT_DIR
+cd $WORK_DIR
+
+#Unmount vhd
+echo "----------------------------------------------------------------"
+echo "Cleaning up"
+echo "----------------------------------------------------------------"
+#sudo guestunmount /mnt/vhd
+#sudo umount /media/iso
+#sh clean.sh
+unset BUILD_DIR
+unset WORK_DIR
+unset OUTPUT_DIR
+
+echo "----------------------------------------------------------------"
+echo "Build Complete"
+echo "----------------------------------------------------------------"
---
+++ debian-install.sh
@@ -0,0 +1,139 @@
+#!/bin/bash
+temp_dir="/install/debian-install"
+tar_file="appliance.tar.gz"
+
+# Need 2CPU
+# Need 4GB RAM
+# Need 25 GB Free Space
+
+echo "----------------------------------------------------------------"
+echo "### Checking Pre-Reqs ###"
+echo "----------------------------------------------------------------"
+
+if [ $EUID -ne 0 ]; then
+   echo "----------------------------------------------------------------"
+   echo "### This script must be run as root ###"
+   echo "----------------------------------------------------------------"
+   exit 1
+fi
+
+FREE=`df -k / --output=avail "$PWD" | tail -n1`   # df -k not df -h
+if [ $FREE -lt 27262976 ]; then               # 26G = 26*1024*1024k
+     # less than 26GBs free!
+     echo "----------------------------------------------------------------"
+     echo "### The installation requires 26GB Free on the root partition (/) ###"
+     echo "----------------------------------------------------------------"
+     exit 1
+fi
+
+CPUS=`getconf _NPROCESSORS_ONLN`
+if [ $CPUS -lt 2 ]; then
+     echo "----------------------------------------------------------------"
+     echo "### WARNING: 2CPUS Required! ###"
+     echo "----------------------------------------------------------------"
+     exit 1
+fi
+
+RAM=`sudo dmidecode -t 17 | grep "Size.*MB" | awk '{s+=$2} END {print s / 1024}' | awk -F'.' '{print $1}'`
+if [ $RAM -lt 4 ]; then
+     echo "----------------------------------------------------------------"
+     echo "### WARNING: 4GB RAM Required! ###"
+     echo "----------------------------------------------------------------"
+     exit 1
+fi
+
+echo "----------------------------------------------------------------"
+echo "### Build Swapfile ###"
+echo "----------------------------------------------------------------"
+sudo dd if=/dev/zero of=/swapfile count=4096 bs=1MB
+sudo chmod 600 /swapfile
+sudo mkswap /swapfile
+sudo swapon /swapfile
+echo '/swapfile swap swap defailts 0 0'|sudo tee -a /etc/fstab
+
+echo "----------------------------------------------------------------"
+echo "### Create Admin Account ###"
+echo "----------------------------------------------------------------"
+sudo adduser -m admin
+sudo usermod -aG wheel admin
+sudo usermod -aG sudo admin
+
+echo "----------------------------------------------------------------"
+echo "### Allow ssh Password Authentication ###"
+echo "----------------------------------------------------------------"
+sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
+systemctl restart sshd
+
+echo "----------------------------------------------------------------"
+echo "### Unzipping arhive and installing files ###"
+echo "----------------------------------------------------------------"
+mkdir $temp_dir
+tar -zxvf $tar_file -C $temp_dir
+cp -R $temp_dir/appliance/loginvsi /
+cp -R $temp_dir/appliance/usr /
+cp -f $temp_dir/appliance/etc/systemd/system/loginvsid.service /etc/systemd/system/
+cp -f $temp_dir/appliance/etc/systemd/system/pi_guard.service /etc/systemd/system/
+systemctl enable pi_guard
+systemctl enable loginvsid
+
+mv $temp_dir/appliance/usr/bin/pdmenu /usr/bin/pdmenu
+
+chmod -R +x /loginvsi/bin/*
+chmod +x /usr/bin/loginvsid
+chown root:root /usr/bin/loginvsid
+
+echo "----------------------------------------------------------------"
+echo "### Uninstalling Docker ###"
+echo "----------------------------------------------------------------"
+#yum update -y
+sh -c "$(curl -fsSL https://get.docker.com)"
+#yum module remove -y container-tools
+
+sudo apt-get remove -y docker \
+                         docker-engine \
+                         docker.io \
+                         containerd \
+                         runc
+
+sudo apt-get purge docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin docker-ce-rootless-extras
+
+#yum install -y yum-utils
+#Install packages
+#rpm -ivh --nodeps $temp_dir/appliance/*.rpm
+#sudo yum install -y device-mapper-persistent-data lvm2
+
+sudo apt-get update
+
+sudo apt-get install \
+     ca-certificates \
+     curl \
+     gnupg \
+     lsb-release
+
+sudo sh get-docker.sh
+
+echo "----------------------------------------------------------------"
+echo "### Starting Docker ###"
+echo "----------------------------------------------------------------"
+systemctl start docker
+systemctl enable docker
+
+
+echo "----------------------------------------------------------------"
+echo "### Installing Docker Compose ###"
+echo "----------------------------------------------------------------"
+curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+
+echo "----------------------------------------------------------------"
+echo "### Initiating docker swarm... ###"
+echo "----------------------------------------------------------------"
+docker swarm init
+docker load -i $temp_dir/appliance/images/*
+
+echo "----------------------------------------------------------------"
+echo "### Perform first run manually - default admin credentials will be set ###"
+echo "as root:"
+echo "sh /loginvsi/bin/firstrun"
+echo "after reboot, reconnect as admin and the installer will finish"
+echo "----------------------------------------------------------------"
